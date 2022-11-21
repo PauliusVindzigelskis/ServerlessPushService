ed69db5b90ba513e610a74fff2fe907e0ee8077e
Paulius Vindzigelskis <paulius.vindzigelskis@kdrp.com>
2022-11-21 15:40:46 -0600

    Source files

---
+++ Serverless.podspec
@@ -0,0 +1,13 @@
+Pod::Spec.new do |s|
+  s.name          = 'ServerlessPushService'
+  s.version       = '0.8.0'
+  s.summary       = 'Framework to play with the Apple Push Notification service (APNs).'
+  s.homepage      = 'https://github.com/PauliusVindzigelskis/ServerlessPushService'
+  s.license       = { :type => 'MIT', :file => 'LICENSE.txt' }
+  s.author        = { 'Paulius Vindzigelskis' => 'p.vindzigelskis@gmail.com' }
+
+  s.ios.deployment_target = '11.0'
+  s.requires_arc  = true
+  s.source        = { :git => 'https://github.com/PauliusVindzigelskis/ServerlessPushService.git', :tag => s.version.to_s }
+  s.source_files  = 'Source/*.{swift}'
+end
---
+++ Source/ServerlessPushService.swift
@@ -0,0 +1,210 @@
+//
+//  ServerlessPushService.swift
+//
+//  Created by Paulius Vindzigelskis on 11/7/22.
+//
+
+import Foundation
+
+class ServerlessPushService: NSObject {
+    
+    enum Priority: Int {
+        case low = 1
+        case medium = 5
+        case immediate = 10
+    }
+    
+    struct Certificate {
+        let name: String
+        let password: String
+        let host: Host
+    }
+    
+    
+    enum Host {
+        case sandbox
+        case production
+        
+        var urlPath: String {
+            switch (self) {
+            case .sandbox: return "https://api.development.push.apple.com"
+            case .production: return "https://api.push.apple.com"
+            }
+        }
+    }
+    
+
+    struct Payload {
+        var title: String
+        var subtitle: String?
+        var body: String
+        var badge: Int?
+        var priority: Priority
+        
+        var targetToken: String
+        var targetBundleID: String
+        
+        
+        static func empty() -> Payload {
+            return Payload(title: "", body: "", priority: .low, targetToken: "", targetBundleID: "")
+        }
+        
+        var json: [String: Any] {
+            var alert: [String:Any] = [
+                "title" : title,
+                "body" : body
+            ]
+            if let subtitle = subtitle {
+                alert["subtitle"] = subtitle
+            }
+            var aps: [String: Any] = [
+                "alert" : alert
+            ]
+            if let badge = badge {
+                aps["badge"] = badge
+            }
+            let payload: [String: Any] = [
+                "aps" : aps
+            ]
+            
+            return payload
+        }
+    }
+    
+    var certificate: Certificate
+    var background = OperationQueue()
+    var session: URLSession!
+    var logError: ( (_ msg: Error) -> () )?
+    let kPushCertificatePrefixes =  ["Apple Sandbox Push Services: ",
+                                     "Apple Development IOS Push Services: ",
+                                     "Apple Production IOS Push Services: ",
+                                     "Apple Development Mac Push Services: ",
+                                     "Apple Production Mac Push Services: ",
+                                     "Apple Push Services: "]
+    
+    init(certificate cert: Certificate) {
+        certificate = cert
+        super.init()
+        
+        session = URLSession(configuration: .default, delegate: self, delegateQueue: background)
+    }
+    
+    typealias Completion = (Error?) -> ()
+    
+    func push(notification: Payload, completion: Completion? = nil ) {
+        let payload = notification.json
+        
+        let urlPath = "\(certificate.host.urlPath)/3/device/\(notification.targetToken)"
+        let url = URL(string: urlPath)!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = try? JSONSerialization.data(withJSONObject: payload)
+        request.addValue("\(notification.priority.rawValue)", forHTTPHeaderField: "apns-priority")
+        request.addValue(notification.targetBundleID, forHTTPHeaderField: "apns-topic")
+        request.addValue("alert", forHTTPHeaderField: "apns-push-type")
+        
+        session.dataTask(with: request) { data, response, error in
+            OperationQueue.main.addOperation { [weak self] in
+                if let error = error {
+                    self?.logError?(error)
+                    completion?(error)
+                    return
+                }
+                
+                if let data = data,
+                   let body = String(data: data, encoding: .utf8),
+                   !body.trim().isEmpty {
+                    let error = NSError(domain: "APNS-Response", code: 0, userInfo: [NSLocalizedDescriptionKey: body])
+                    self?.logError?(error)
+                    completion?(error)
+                } else {
+                    completion?(nil)
+                }
+            }
+            
+        }.resume()
+    }
+    
+    func isPushCertificate(certificate: SecCertificate) -> Bool {
+        guard let descriptionCF = SecCertificateCopySubjectSummary(certificate) else {
+            return false
+        }
+        
+        let description = descriptionCF as String
+        
+        for prefix in kPushCertificatePrefixes {
+            if description.hasPrefix(prefix) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    func identities(certificate: Certificate) -> [AnyObject] {
+        guard let url = Bundle.main.url(forResource: certificate.name, withExtension: "p12"),
+              let certData = try? Data(contentsOf: url) as NSData
+        else { return [] }
+        
+        let options: NSDictionary = [kSecImportExportPassphrase as NSString: certificate.password]
+        var items : CFArray?
+        
+        let securityError: OSStatus = SecPKCS12Import(certData, options, &items)
+        
+        if let theArray = items,
+           securityError == noErr && CFArrayGetCount(theArray) > 0 {
+            let newArray = theArray as NSArray
+            return newArray as [AnyObject]
+        }
+        
+        return []
+    }
+    
+    func certificatesWithIdentity(identity: SecIdentity) -> SecCertificate? {
+        var certificateRef: SecCertificate? = nil
+        let securityError = SecIdentityCopyCertificate(identity , &certificateRef)
+        if securityError != noErr {
+            certificateRef = nil
+        }
+        
+        return certificateRef
+    }
+    
+    func signature() -> (idenity: SecIdentity, certificate: SecCertificate)? {
+        
+        let identities = identities(certificate: certificate)
+        for dict in identities {
+            if let temp = dict[kSecImportItemIdentity] {
+                let identity = temp as! SecIdentity
+                let cert: SecCertificate! = certificatesWithIdentity(identity: identity)
+                if cert == nil {
+                    return nil
+                }
+                
+                if !isPushCertificate(certificate: cert) {
+                    return nil
+                }
+                
+                return (identity, cert)
+            }
+        }
+        
+        return nil
+    }
+    
+    
+}
+
+extension ServerlessPushService: URLSessionDelegate {
+    func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+        
+        guard let signature = signature() else {
+            let error = NSError(domain: "ServerlessPushService", code: 0, userInfo: [NSLocalizedDescriptionKey: "Unable to setup signature from Push Certification (p12) file"])
+            logError?(error)
+            completionHandler(.cancelAuthenticationChallenge, nil)
+            return
+        }
+        
+        let cred = URLCredential(identity: signature.idenity, certificates: [signature.certificate], persistence: .forSession)
+        completionHandler(.useCredential, cred)
+    }
+}
